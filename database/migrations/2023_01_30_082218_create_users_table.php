e54958a364c9e8d09e1b317826077eaa2bc1ffe1
zalobmq <gbretonesmoraquero@gmail.com>
2023-01-31 10:02:24 +0100

    1:n relationships created

--- database/migrations/2023_01_30_082218_create_users_table.php
+++ database/migrations/2023_01_30_082218_create_users_table.php
@@ -14,7 +14,7 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->string('DNI');
+            $table->string('DNI')->primary();
             $table->integer('Rol');
             $table->date('Course_year');
             $table->string('Cycle');
