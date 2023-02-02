7b8b3ff7a46dd29e656556f9e09dc15a2ec340a8
zalobmq <gbretonesmoraquero@gmail.com>
2023-02-02 13:11:06 +0100

    controller appointment

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\AppointmentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,27 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::controller(AppointmentController::class) -> group(function(){
+
+    /*___________________________________________________________________________________________________________________ */
+
+                                                    /*  GET */
+    Route::get('/appointments', 'getAll');
+    Route::get('/appointment/{id}', 'getAppointmentById');
+    Route::get('/appointment/dni/client/{DNI_client}', 'getAppointmentByDNIClient');
+    Route::get('/appointment/dni/student/{DNI_Student}', 'getClientByDniStudent');
+/*___________________________________________________________________________________________________________________ */
+
+                                                    /*  POST */
+    Route::post('/appointment/add', 'addAppointment');
+    /*___________________________________________________________________________________________________________________ */
+
+                                                    /*  PUT */
+    Route::put('/appointment/edit/{id}', 'editAppointment');
+    /*___________________________________________________________________________________________________________________ */
+
+                                                    /*  DELETE */
+    Route::delete('/appointment/{id}', 'DeleteAppointmenById');
+    Route::delete('/delete-all', 'deleteAll');
 });
