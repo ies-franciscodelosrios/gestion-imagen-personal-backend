b86a5499046e3607f4bfa46cda0c5e2432da90eb
Chelu97 <pepeluis.cc@gmail.com>
2023-02-03 11:15:27 +0100

    Errors corrections in the api routes and a missed addon in the client controller

--- routes/api.php
+++ routes/api.php
@@ -31,6 +31,56 @@ Route::controller(AppointmentController::class) -> group(function(){
     Route::get('/appointment/{id}', 'getAppointmentById');
     Route::get('/appointment/dni/client/{DNI_client}', 'getAppointmentByDNIClient');
     Route::get('/appointment/dni/student/{DNI_Student}', 'getClientByDniStudent');
+    /*___________________________________________________________________________________________________________________ */
+
+                                                    /*  POST */
+    Route::post('/appointment/add', 'addAppointment');
+    /*___________________________________________________________________________________________________________________ */
+
+                                                    /*  PUT */
+    Route::put('/appointment/edit/{id}', 'editAppointment');
+    /*___________________________________________________________________________________________________________________ */
+
+                                                    /*  DELETE */
+    Route::delete('/appointment/{id}', 'DeleteAppointmenById');
+    Route::delete('/appointment/delete-all', 'deleteAll');
+
+});
+
+Route::controller(ClientController::class) -> group(function(){
+
+    /*___________________________________________________________________________________________________________________ */
+
+                                                    /*  GET */
+
+    Route::get('/clients', 'getClientAll'); /* Working */
+    Route::get('/client/{id}', 'searchClient');/* Working */
+/*___________________________________________________________________________________________________________________ */
+
+                                                    /*  POST */
+
+    Route::post('/client/add', 'addClient');/* Working */
+    /*___________________________________________________________________________________________________________________ */
+
+                                                    /*  PUT */
+
+    Route::put('/client/edit/{id}', 'editById');/* Working */
+    /*___________________________________________________________________________________________________________________ */
+
+                                                    /*  DELETE */
+
+    Route::delete('/client/delete/{id}', 'deleteById');/* Working */
+    Route::delete('/client/deleteall', 'deleteAll');/* Not Working */
+
+});
+
+
+Route::controller(UsersController::class) -> group(function(){
+
+     /*___________________________________________________________________________________________________________________ */
+
+                                                    /*  GET */
+
     Route::get('/users','getAll');
     Route::get('/users/rol/2','getAllStudents');
     Route::get('/users/rol/1','getAllProfessor');
@@ -38,34 +88,29 @@ Route::controller(AppointmentController::class) -> group(function(){
     Route::get('/user/Student/{Name}','getUserByName');
     Route::get('/users/course/{Course_year}','getUserByCourse');
     Route::get('/users/cycle/{Cycle}','getUserByCycle');
-    Route::get('/clients', 'getClientAll'); /* Working */
-    Route::get('/client/{id}', 'searchClient');/* Working */
+
 /*___________________________________________________________________________________________________________________ */
 
                                                     /*  POST */
-    Route::post('/appointment/add', 'addAppointment');
+
     Route::post('/user/addstudent','addStudent');
     Route::post('/user/addprofessor','addProfessor');
     Route::post('/user/addstudents','addAllStudent');
     Route::post('/user/addprofessors','addAllProfessor');
     Route::post('/user/login','loginUser');
-    Route::post('/client/add', 'addClient');/* Working */
+
     /*___________________________________________________________________________________________________________________ */
 
                                                     /*  PUT */
-    Route::put('/appointment/edit/{id}', 'editAppointment');
+
     Route::put('/user/edit/{id}','editUser');
-    Route::put('/client/edit/{id}', 'editById');/* Working */
+
     /*___________________________________________________________________________________________________________________ */
 
                                                     /*  DELETE */
-    Route::delete('/appointment/{id}', 'DeleteAppointmenById');
-    Route::delete('/appointment/delete-all', 'deleteAll');
+
     Route::delete('/user/delete/{dni}','deleteUser');
     Route::delete('/user/delete/rol/{rol}','deleteByRol');
     Route::delete('/user/deleteall/rol','deleteAllByRol');
-    Route::delete('/client/delete/{id}', 'deleteById');/* Working */
-    Route::delete('/client/deleteall', 'deleteAll');
 
 });
-
