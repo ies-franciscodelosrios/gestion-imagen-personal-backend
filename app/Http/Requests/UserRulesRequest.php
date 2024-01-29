deedfd5ec39f9a0a8cec9882e7164f1d71c0b84b
Juanmagw <juanma4x21@gmail.com>
2024-01-29 13:18:06 +0100

    TC collections, editUser URL modified and UserRulesRequest password not obligatory

--- app/Http/Requests/UserRulesRequest.php
+++ app/Http/Requests/UserRulesRequest.php
@@ -21,7 +21,7 @@ class UserRulesRequest extends FormRequest
             'name' => 'required|min:3|max:50|regex:/^[A-Z]/',
             'surname' => 'required|min:3|max:50|regex:/^[A-Z]/',
             'email' => 'required|email|max:50',
-            'password' => 'required|min:8|max:50',
+            'password' => 'min:8|max:50',
             'course_year' => 'required|min:1|max:5',
             'cycle' => 'required|integer'
         ];
