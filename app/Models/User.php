8348e3c7fbfd9017d5a2d21c7f630e37a77f48a6
juanmacasgim <juanmariacastillogimenez@gmail.com>
2024-02-07 11:16:43 +0100

    Depuration encript password of new users

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,6 +40,7 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'password' => 'hashed',
     ];
 
     public function photoUrls()
